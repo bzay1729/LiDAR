4ca4cf35808074d3e989ce133cf7017f89917f84
yajib16 <50978166+yajib16@users.noreply.github.com>
2026-04-03 14:20:30 -0500

    Added color red for fake points and gray for original

--- src/main.py
+++ src/main.py
@@ -5,18 +5,24 @@ from load_kitti import load_kitti_bin
 from inject_fake_points import generate_fake_points, inject_fake_points
 
 
-def visualize_points(points: np.ndarray, title: str = "Point Cloud"):
+def visualize_with_colors(original_points, fake_points):
     """
-    Visualize a point cloud in Open3D.
+    Visualize original and fake points using different color.
     """
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.points = o3d.utility.Vector3dVector(points)
-    o3d.visualization.draw_geometries(
-        [point_cloud],
-        window_name=title,
-        width=900,
-        height=700
-    )
+    # Combine points
+    all_points = np.vstack((original_points, fake_points))
+
+    # Creating colors
+    original_colors = np.tile([0.5, 0.5, 0.5], (original_points.shape[0], 1)) # gray color
+    fake_colors = np.tile([1.0, 0.0, 0.0], (fake_points.shape[0], 1))  # red color
+    all_colors = np.vstack((original_colors, fake_colors))
+
+    # Creating Open3D object
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(all_points)
+    pcd.colors = o3d.utility.Vector3dVector(all_colors)
+
+    o3d.visualization.draw_geometries([pcd], window_name="Fake Point Injection")
 
 
 if __name__ == "__main__":
@@ -38,15 +44,17 @@ if __name__ == "__main__":
     }
 
     # Fake points Generations
-    fake_points = generate_fake_points(target_box, num_points=300)
+    fake_points = generate_fake_points(target_box, num_points=500)
     print("Fake points shape:", fake_points.shape)
 
     # Inject fake points
     attacked_points = inject_fake_points(original_points, fake_points)
     print("Attacked point cloud shape:", attacked_points.shape)
 
+    # Visualize original and fake points with colors
+    visualize_with_colors(original_points, fake_points)
     # Visualize original scene
-    visualize_points(original_points, title="Original Point Cloud")
+    #visualize_points(original_points, title="Original Point Cloud")
 
     # Visualize attacked scene
-    visualize_points(attacked_points, title="Attacked Point Cloud")
+    #visualize_points(attacked_points, title="Attacked Point Cloud")
